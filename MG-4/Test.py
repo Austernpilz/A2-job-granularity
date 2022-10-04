4ed53d4b86880c7667b96743da6e61a3f90b3a0a
Austernpilz <manuel.zsch@gmail.com>
2022-10-04 16:31:28 +0200

    work in progress

--- MG-4/Test.py
+++ MG-4/Test.py
@@ -1,22 +1,25 @@
 #Testfile for changing Parameters in the Workflow
 
 import subprocess
+import pandas as pd
+import numpy as np 
 
-cmd = "sinfo | grep -P "alloc|mix" | grep -P "big" | awk '{print $6}'"
-
-# def NodeList():
-
-subprocess.run(cmd)
-
-# def change_config
-
-#     with open ('../simulation_config.yaml', 'w') as f:
-#         config_file = yaml.load(f)
-#         config_file['number_of_bins'] = n  
+cmd = ["bash", "-c", "sinfo -N | grep -P \"alloc|mix\" | grep -P big | awk '{print $1}'"]
+def NodeList():
+    result = subprocess.run(cmd)
 
 
+def change_config
 
-#for n in range(NodeList.length()):
+    with open ('../simulation_config.yaml', 'w') as f:
+        config_file = yaml.load(f)
+        config_file['number_of_bins'] = n  
 
+
+
+for n in range(NodeList.length()):
+
+
+subprocess.run(["bash", "-c", "sinfo | grep -P \"alloc|mix\" | grep -P big | awk '{print $6}'"])
 
 
