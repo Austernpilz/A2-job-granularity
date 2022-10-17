ff7a2197c7b61be2c4f644218a8856e9ebe6b723
Austernpilz <manuel.zsch@gmail.com>
2022-10-17 14:08:59 +0200

    testing intestifies

--- MG-4/Test.py
+++ MG-4/Test.py
@@ -4,22 +4,32 @@ import subprocess
 import pandas as pd
 import numpy as np 
 
-cmd = ["bash", "-c", "sinfo -N | grep -P \"alloc|mix\" | grep -P big | awk '{print $1}'"]
-def NodeList():
-    result = subprocess.run(cmd)
+def NodeList_offline():
+    return pd.read_csv("../../NodeList.csv", header=0, index_col=False)
 
 
-def change_config
-
-    with open ('../simulation_config.yaml', 'w') as f:
-        config_file = yaml.load(f)
-        config_file['number_of_bins'] = n  
+def function (bin_nr):
+    list_of_Nodes = NodeList_offline()
+    return list_of_Nodes(NodeName[bin_nr])
 
 
+# def NodeList():
+#     cmd = ["bash", "-c", "sinfo -N"]        #| grep -P \"alloc|mix\" | grep -P big | awk '{print $1}'"
+#     result = subprocess.run(cmd, stdout=subprocess.PIPE)
+#     Nodepanda = pd.read(result.decode('utf-8'))
+#     return Nodepanda
 
-for n in range(NodeList.length()):
+# def change_config()
+
+#     with open ('../simulation_config.yaml', 'w') as f:
+#         config_file = yaml.load(f)
+#         config_file['number_of_bins'] = n  
 
 
-subprocess.run(["bash", "-c", "sinfo | grep -P \"alloc|mix\" | grep -P big | awk '{print $6}'"])
+
+# for n in range(NodeList().length()):
+
+
+# subprocess.run(["bash", "-c", "sinfo | grep -P \"alloc|mix\" | grep -P big | awk '{print $6}'"])
 
 
